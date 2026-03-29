51eb574474c2acc8d358a12d46698b0ae09106d7
Gabriel dos Santos <72713078+gabe1007@users.noreply.github.com>
2026-03-29 10:17:06 -0300

    Refactor hash table implementation and add functions

--- data_structures/hash_table.c
+++ data_structures/hash_table.c
@@ -1,38 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 typedef struct Node {
     char key[256];
     int value;
     struct Node *next;
-}Node;
+} Node;
 
 typedef struct Table {
     int tamanho;
     Node **vetor;
-}Table;
+} Table;
 
-unsigned long djb2_hash(unsigned char *str);
+void print(int tamanho, Table *dicio);
+void insert(Table *dicio, char *chave, int value);
+unsigned long djb2_hash(unsigned char *str, int tamanho);
+void search(char *search_key, Table *dicio);
+void remover(char *search_key, Table *dicio);
 
 int main() {
-    int tamanho  = 10;
-    Table *new   = malloc(sizeof(Table));
-    new->tamanho = tamanho;
-    new->vetor   = calloc(tamanho, sizeof(Node *));
+    int tamanho          = 15;
 
-    char string[256] = "Nome";
-    unsigned long hash = djb2_hash((unsigned char *)string);
-    printf("Hash value for '%s': %lu\n", string, hash);
+    Table *dicio         = malloc(sizeof(Table));
+    dicio->tamanho       = tamanho;
+    dicio->vetor         = calloc(tamanho, sizeof(Node *));
+
+    char chave[256] = "Idade";
+    int idade = 35;
+    insert(dicio, chave, idade);
+
+    char chave_[256] = "Tamanho";
+    int idade_ = 42;
+    insert(dicio, chave_, idade_);
+
+    char mulher[256] = "Sim";
+    int size = 165;
+    insert(dicio, mulher, size);
+
+    print(tamanho, dicio);
+
+    char search_key[256] = "Idade";
+    // search(search_key, dicio);
+    remover(search_key, dicio);
+    print(tamanho, dicio);
+
 }
 
-unsigned long djb2_hash(unsigned char *str) {
+void remover(char *search_key, Table *dicio) {
+    unsigned long hash_value;
+    Node *prev = NULL;
+
+    hash_value = djb2_hash((unsigned char *)search_key, dicio->tamanho);
+    Node *current = dicio->vetor[hash_value];
+    if (current == NULL) {
+        printf("A chave \"%s\" não existe na tabela\n", search_key);
+    } else {
+        while (current != NULL) {
+            if (strcmp(current->key, search_key) == 0) {
+                Node *tmp = current->next;
+                free(current);
+                if (prev == NULL) {
+                    dicio->vetor[hash_value] = tmp;
+                } else {
+                    prev->next = tmp;
+                }
+                break;
+            }
+            prev    = current;
+            current = current->next;
+        }
+    }
+}
+
+void search(char *search_key, Table *dicio) {
+    unsigned long hash_value;
+
+    hash_value = djb2_hash((unsigned char *)search_key, dicio->tamanho);
+    Node *current = dicio->vetor[hash_value];
+    if (current == NULL) {
+        printf("A chave \"%s\" não existe na tabela\n", search_key);
+    }
+    while (current != NULL) {
+        if (strcmp(current->key, search_key) == 0){
+            printf("Valor encontrado %s: %d\n", current->key, current->value);
+            break;
+        }
+        current = current->next;
+    }    
+}
+
+void insert(Table *dicio, char *chave, int value) {
+    unsigned long hash_value;
+
+    hash_value = djb2_hash((unsigned char *)chave, dicio->tamanho);
+    Node *new = malloc(sizeof(Node));
+    strcpy(new->key, chave);
+    new->value = value;
+    new->next = dicio->vetor[hash_value];
+    dicio->vetor[hash_value] = new;
+}
+
+unsigned long djb2_hash(unsigned char *str, int tamanho) {
     unsigned long hash = 5381;
     int c;
 
     while ((c = *str++)) {
         hash = ((hash << 5) + hash) + c;
-        printf("Current hash value: %lu\n", hash);
     }
 
-    return hash;
+    return hash % tamanho;
+}
+
+void print(int tamanho, Table *dicio) {
+    for (int i = 0; i < tamanho; i++) {
+        Node *current = dicio->vetor[i];
+        while (current != NULL) {
+            printf("%s: %d\n", current->key, current->value);
+            current = current->next;
+        }
+    }
 }
